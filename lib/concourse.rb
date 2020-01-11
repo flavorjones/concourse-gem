46af071a1ebd2959560facc20943579f1cae19fb
Mike Dalessio <mike.dalessio@gmail.com>
2020-01-11 14:04:06 -0500

    remove jruby 9.1
    (because ruby 2.3 is out of support)

--- lib/concourse.rb
+++ lib/concourse.rb
@@ -12,7 +12,7 @@ class Concourse
   # these numbers/names align with public docker image names
   RUBIES = {
     mri: %w[2.4 2.5 2.6 2.7], # docker repository: "ruby"
-    jruby: %w[9.1 9.2], # docker repository: "jruby"
+    jruby: %w[9.2], # docker repository: "jruby"
     rbx: %w[latest], # docker repository: "rubinius/docker"
     windows: %w[2.3 2.4 2.5 2.6], # windows-ruby-dev-tools-release
   }
