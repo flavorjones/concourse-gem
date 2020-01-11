65374e59ed0b65784b5cbadae7195377f8257ef8
Mike Dalessio <mike.dalessio@gmail.com>
2020-01-11 13:36:32 -0500

    remove ruby 2.3 which has reached EOL. add 2.7.
    see this link for background:
    - https://www.ruby-lang.org/en/news/2019/03/31/support-of-ruby-2-3-has-ended/

--- lib/concourse.rb
+++ lib/concourse.rb
@@ -11,10 +11,10 @@ class Concourse
 
   # these numbers/names align with public docker image names
   RUBIES = {
-    mri: %w[2.3 2.4 2.5 2.6 2.7-rc], # docker repository: "ruby"
-    jruby: %w[9.1 9.2],     # docker repository: "jruby"
-    rbx: %w[latest],      # docker repository: "rubinius/docker"
-    windows: %w[2.3 2.4 2.5 2.6],  # windows-ruby-dev-tools-release
+    mri: %w[2.4 2.5 2.6 2.7], # docker repository: "ruby"
+    jruby: %w[9.1 9.2], # docker repository: "jruby"
+    rbx: %w[latest], # docker repository: "rubinius/docker"
+    windows: %w[2.3 2.4 2.5 2.6], # windows-ruby-dev-tools-release
   }
 
   DEFAULT_DIRECTORY = "concourse"
